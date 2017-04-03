0046e4478db61a128d225e2333256dbd6b5c2eed
Diana ARTIOM <diana.artiom24@gmail.com>
2017-04-03 23:09:29 +0200

    Added project.

---
+++ src/main/java/com/deusto/BookitApplication.java
@@ -0,0 +1,15 @@
+package com.deusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+public class BookitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookitApplication.class, args);
+	}
+}
