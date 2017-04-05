658016d53ca73d694d30807b7c3004e731341233
Vovc Artiom <vovc.artiom@gmail.com>
2017-04-06 02:54:10 +0300

    added swagger 2

--- src/main/java/com/deusto/BookitApplication.java
+++ src/main/java/com/deusto/BookitApplication.java
@@ -1,12 +1,15 @@
 package com.deusto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 
 @SpringBootApplication
 @EnableEmailTools
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class BookitApplication {
 
 	public static void main(String[] args) {
