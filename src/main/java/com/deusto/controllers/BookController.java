4199ca4230a34186939d1405d2314c342bcdea2b
Diana Artiom <diana.artiom@faf.utm.md>
2017-04-14 16:43:52 +0300

    fix

--- src/main/java/com/deusto/controllers/BookController.java
+++ src/main/java/com/deusto/controllers/BookController.java
@@ -2,8 +2,6 @@ package com.deusto.controllers;
 
 import com.deusto.builders.BookBuilder;
 import com.deusto.dtos.BookDTO;
-import com.deusto.models.Book;
-import com.deusto.repositories.BookRepository;
 import com.deusto.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -11,12 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
 
 @Controller
+@RequestMapping(path = "/book")
 public class BookController {
 
     @Autowired
@@ -28,19 +26,19 @@ public class BookController {
 //    }
 
     /* pentru mine ca sa pot introduce carti */
-    @PostMapping(path = "/book", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public HttpEntity<?> createBook(@RequestBody BookDTO bookDTO) {
 
         return new ResponseEntity(bookService.insert(BookBuilder.get(bookDTO)), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/book/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public HttpEntity<?> getBookById(@PathVariable String id) {
         return new ResponseEntity(bookService.findById(id), OK);
     }
 
     /* trial pentru parametri de filtrare */
-    @GetMapping(path = "/book", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public HttpEntity<?> getAllBooksFilter(@RequestParam(value = "title") String title) {
         return new ResponseEntity(bookService.findByTitle(title), OK);
     }
