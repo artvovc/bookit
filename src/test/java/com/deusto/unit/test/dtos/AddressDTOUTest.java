bda23f19809628154f79fcad8b0031add6017930
Diana ARTIOM <diana.artiom24@gmail.com>
2017-05-05 22:43:19 +0200

    Added DTO Unit Testing.

--- src/test/java/com/deusto/unit/test/dtos/AddressDTOUTest.java
+++ src/test/java/com/deusto/unit/test/dtos/AddressDTOUTest.java
@@ -1,8 +1,34 @@
 package com.deusto.unit.test.dtos;
 
+import com.deusto.dtos.AddressDTO;
 import com.deusto.unit.test.common.AbstractUT;
-import org.junit.Ignore;
+import org.junit.Test;
 
-@Ignore
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class AddressDTOUTest extends AbstractUT {
+
+    @Test
+    public void validBookDTO() {
+        AddressDTO addressDTO = new AddressDTO();
+
+        addressDTO.setCountry("Moldova");
+        addressDTO.setStreet("Stephan the Great");
+        addressDTO.setCity("Chisinau");
+        addressDTO.setApartamentNumber(123);
+        addressDTO.setHouseNumber(666);
+        addressDTO.setPostalCode("4822");
+
+        assertTrue((validate(addressDTO).isEmpty()));
+
+        assertThat(addressDTO.getCity(), is("Chisinau"));
+        assertThat(addressDTO.getStreet(), is("Stephan the Great"));
+        assertThat(addressDTO.getCountry(), is("Moldova"));
+        assertThat(addressDTO.getApartamentNumber(), is(123));
+        assertThat(addressDTO.getHouseNumber(), is(666));
+        assertThat(addressDTO.getPostalCode(), is("4822"));
+    }
 }
