e4dba9a8f1672408fcd99efe28b460915fa6f562
Diana ARTIOM <janireuriarte@opendeusto.es>
2017-05-05 16:51:07 +0200

    Wrote tests for LoginController.

--- src/main/java/com/deusto/repositories/BookRepository.java
+++ src/main/java/com/deusto/repositories/BookRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Repository
 public interface BookRepository extends MongoRepository<Book,String>{
+	Book findAllById(String id);
 	List<Book> findAll();
 	List<Book> findByTitle(String title);
 	Book findBookById(String id);
